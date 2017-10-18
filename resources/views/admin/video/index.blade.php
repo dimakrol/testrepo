71799b9fc8008acefdf6d57acafddbd6d776cf1f
dimakrol <dima.krol@mail.ru>
2017-10-19 02:16:29 +0300

    Added no video yet.

--- resources/views/admin/video/index.blade.php
+++ resources/views/admin/video/index.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.admin.app')
 @section('admin-content')
+@if($videos->count() > 0)
 <table class="table">
     <thead class="thead-default">
     <tr>
@@ -10,15 +11,18 @@
     </tr>
     </thead>
     <tbody>
-    @foreach($videos as $video)
-        <tr>
-            <th scope="row">{{$video->id}}</th>
-            <td>{{$video->name}}</td>
-            <td>No slug yet</td>
-            <td><a href="{{ route('admin.video.edit', $video->id) }}"><i class="fa fa-pencil-square-o fa-lg" aria-hidden="true"></i></a></td>
-        </tr>
-    @endforeach
+        @foreach($videos as $video)
+            <tr>
+                <th scope="row">{{$video->id}}</th>
+                <td>{{$video->name}}</td>
+                <td>No slug yet</td>
+                <td><a href="{{ route('admin.video.edit', $video->id) }}"><i class="fa fa-pencil-square-o fa-lg" aria-hidden="true"></i></a></td>
+            </tr>
+        @endforeach
     </tbody>
 </table>
+@else
+<h3>No videos yet</h3>
+@endif
 {{ $videos->links() }}
 @endsection
