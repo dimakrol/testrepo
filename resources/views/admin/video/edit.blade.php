8cbf1526e2e4edb73582150416f4d2fadaaeb556
dmitriyk-hyuna <dmitriy.krol@hyuna.bb>
2017-10-19 15:50:17 +0300

    some style fix.

--- resources/views/admin/video/edit.blade.php
+++ resources/views/admin/video/edit.blade.php
@@ -4,28 +4,32 @@
     @foreach($errors->all() as $message)
         {{$message}}
     @endforeach
-    <h2>Update video {{ $video->name }}</h2>
+    <div class="col-md-6 col-xs-12">
+        <h2>Update video {{ $video->name }}</h2>
+    </div>
+    <div class="col-md-6 col-xs-12">
         <video src="{{ asset('storage/'.$video->local_url) }}" width="400" controls>
             <source width="320" height="240" id="video_here">
             Your browser does not support HTML5 video.
         </video>
+    </div>
     {!! Form::open(['route' => ['admin.video.update', $video->id], 'method'  => 'put', 'files' => true]) !!}
-    <div class="form-group col-md-6">
+    <div class="form-group col-md-6 col-xs-12">
         <label for="inputName" class="col-form-label">Name:</label>
         {!! Form::text('name', $video->name, ['class' => 'form-control', 'id' => 'inputName' , 'placeholder' => 'Video Name', 'required' => 'required']) !!}
     </div>
-    <div class="form-group">
+    <div class="form-group col-md-6 col-xs-12">
         <div class="form-check">
             <label class="form-check-label">
                 {!! Form::checkbox('premium', null, $video->premium, ['class' => 'form-check-input']) !!} Premium
             </label>
         </div>
     </div>
-    <div class="form-group">
+    <div class="form-group col-md-6 col-xs-12">
         <label for="inputVideo">Select video to change</label>
         {!! Form::file('video', ['class' => 'form-control-file', 'id' => 'inputVideo' , 'accept' => 'video/*']) !!}
     </div>
-    <div class="form-group col-md-6">
+    <div class="form-group col-md-6 col-xs-12">
         {!! Form::select('category_id', $categories, $video->category_id, ['placeholder' => 'Select category', 'class' => 'form-control', 'required' => 'required']) !!}
     </div>
     <div class="row">
