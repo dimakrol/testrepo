7d0ec0c70a5250f90959911888e6d8d9d370449a
dmitriyk-hyuna <dmitriy.krol@hyuna.bb>
2017-11-09 15:50:40 +0200

    Added additional relations.

--- app/Models/GeneratedField.php
+++ app/Models/GeneratedField.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class GeneratedField extends Model
 {
-    //
+    public function videoGenerated()
+    {
+        return $this->belongsTo(VideoGenerated::class, 'generated_id');
+    }
 }
--- app/Models/VideoGenerated.php
+++ app/Models/VideoGenerated.php
@@ -8,14 +8,27 @@ class VideoGenerated extends Model
 {
     protected $table = 'videos_generated';
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function video()
     {
         return $this->belongsTo(Video::class, 'video_id');
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function generatedFields()
+    {
+        return $this->hasMany(GeneratedField::class, 'generated_id');
     }
 }
