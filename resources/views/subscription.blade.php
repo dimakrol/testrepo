9079167d002f156ee694a3075bece3c445836016
dmitriyk-hyuna <dmitriy.krol@hyuna.bb>
2017-11-21 19:25:15 +0200

    Set up production subscription.

--- resources/views/subscription.blade.php
+++ resources/views/subscription.blade.php
@@ -100,13 +100,14 @@
                             <h3 class="text-center"><i class="fa fa-cc-paypal fa-2x" aria-hidden="true"></i></h3>
                         </div>
                         <div class="row mx-auto">
-                            <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top" class="paypal-button">
-                                <input type="hidden" name="custom" value="{{json_encode(array('user_id' => 3))}}">
+                            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top"  class="paypal-button">
+                                <input type="hidden" name="custom" value="{{json_encode(array('user_id' => Auth::user()->id))}}">
                                 <input type="hidden" name="cmd" value="_s-xclick">
-                                <input type="hidden" name="hosted_button_id" value="H46Z6LRPBZGQC">
-                                <input type="image" src="https://www.sandbox.paypal.com/en_US/GB/i/btn/btn_subscribeCC_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online!">
-                                <img alt="" border="0" src="https://www.sandbox.paypal.com/en_GB/i/scr/pixel.gif" width="1" height="1">
+                                <input type="hidden" name="hosted_button_id" value="CZQU2CJKR5VG2">
+                                <input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_subscribeCC_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online!">
+                                <img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
                             </form>
+
                         </div>
                     </div>
                     @endif
