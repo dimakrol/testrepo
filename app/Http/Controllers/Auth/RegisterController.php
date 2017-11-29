e8845dffd7493e3f2e5a72749dbea4532e1ed5c9
dmitriyk-hyuna <dmitriy.krol@hyuna.bb>
2017-11-29 19:31:16 +0200

    Added log.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,6 +67,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        Log::debug('Server '.$_SERVER['REMOTE_ADDR']);
         $user = User::create([
             'first_name' => $data['name'],
             'email' => $data['email'],
