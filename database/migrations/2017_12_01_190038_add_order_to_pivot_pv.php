45eda5b2e8afe7504913987807b9f6e35b1ae898
dimakrol <dima.krol@mail.ru>
2017-12-01 21:04:41 +0200

    Updated name of migration,

--- database/migrations/2017_12_01_190038_add_order_to_pivot_pv.php
+++ database/migrations/2017_12_01_190038_add_order_to_pivot_pv.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddOrderColumnToPlaylists extends Migration
+class AddOrderToPivotPv extends Migration
 {
     /**
      * Run the migrations.
