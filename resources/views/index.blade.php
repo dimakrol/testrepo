7af035a42a8f21777ffbc32f27c2c2637977dd66
dmitriyk-hyuna <dmitriy.krol@hyuna.bb>
2017-12-04 12:50:17 +0200

    Fixed error with no videos in playlist.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,30 +18,31 @@
   <div class="container">
       <!-- Page Heading -->
           @foreach($playlists as $playlist)
-          <h1 class="my-4" style="text-decoration: underline; text-decoration-color: #5DC09C">{{$playlist->name}}</h1>
-          <div class="row">
-            @foreach($playlist->videos->chunk(3)[0] as $video)
-                  <div class="col-lg-4 col-sm-6 portfolio-item">
-                      <div class="card h-100">
-                          <a href="{{ route('video.show', $video->slug) }}"><img class="card-img-top" src="{{ $video->getThumbnail() }}" alt=""></a>
-                          <div class="card-body">
-                              <div class="row">
-                                  <div class="col-3">
-                                      <a href="{{ route('video.show', $video->slug) }}"><img src="{{ $video->user->thumbnail_path }}" class="rounded-circle img-fluid"></a>
-                                  </div>
-                                  <div class="col-9">
-                                      <h4>
-                                          <a href="{{ route('video.show', $video->slug) }}">{{ $video->name }}</a>
-                                      </h4>
-                                      <p class="card-text">Created by: <a href="{{ route('channel.index', $video->user->slug) }}">{{$video->user->fullName()}}</a></p>
+              @if($playlist->videos->count())
+                  <h1 class="my-4" style="text-decoration: underline; text-decoration-color: #5DC09C">{{$playlist->name}}</h1>
+                  <div class="row">
+                    @foreach($playlist->videos->chunk(3)[0] as $video)
+                          <div class="col-lg-4 col-sm-6 portfolio-item">
+                              <div class="card h-100">
+                                  <a href="{{ route('video.show', $video->slug) }}"><img class="card-img-top" src="{{ $video->getThumbnail() }}" alt=""></a>
+                                  <div class="card-body">
+                                      <div class="row">
+                                          <div class="col-3">
+                                              <a href="{{ route('video.show', $video->slug) }}"><img src="{{ $video->user->thumbnail_path }}" class="rounded-circle img-fluid"></a>
+                                          </div>
+                                          <div class="col-9">
+                                              <h4>
+                                                  <a href="{{ route('video.show', $video->slug) }}">{{ $video->name }}</a>
+                                              </h4>
+                                              <p class="card-text">Created by: <a href="{{ route('channel.index', $video->user->slug) }}">{{$video->user->fullName()}}</a></p>
+                                          </div>
+                                      </div>
                                   </div>
                               </div>
                           </div>
-                      </div>
+                      @endforeach
                   </div>
-              @endforeach
-          </div>
-
+              @endif
       @endforeach
           {{--@foreach($videos as $video)--}}
               {{--<div class="col-lg-4 col-sm-6 portfolio-item">--}}
