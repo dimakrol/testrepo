2a71d682746bc4e77940dceabfa56c3119816ebd
Artem Slepets <49h4DhPERFECT@gmail.com>
2017-12-05 00:15:14 +0200

    Header: 1) hamburger button has been removed; 2) drop down menu requires only one click now

--- resources/views/layouts/navigation/main.blade.php
+++ resources/views/layouts/navigation/main.blade.php
@@ -1,18 +1,15 @@
 <!-- Navigation -->
-<nav class="navbar navbar-expand-lg fixed-top">
+<nav class="navbar navbar-expand fixed-top">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">Words Won't Do</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
                 @guest
                     <li class="nav-item">
-                        <a href="{{ route('register') }}" class="btn btn-success">Sign Up</a>
+                        <a href="{{ route('register') }}" class="btn btn-success hidden-xs-down">Sign Up</a>
                     </li>
                     <li class="nav-item">
-                        <a href="{{ route('login') }}" class="btn btn-light menu-login-button">Login</a>
+                        <a href="{{ route('login') }}" class="btn btn-light menu-login-button mr-0">Login</a>
                     </li>
                 @else
                     <div class="dropdown" role="menu">
