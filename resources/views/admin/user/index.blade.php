d51ac94873eb16935db1ba77681ce3153e3de1fe
dimakrol <dima.krol@mail.ru>
2017-12-05 23:52:43 +0200

    Added on admin dushboard

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -13,6 +13,7 @@
             <th>Role</th>
             <th>Subscription</th>
             <th>Signed Up</th>
+            <th>Lust Sign In</th>
             <th></th>
             <th></th>
             <th></th>
@@ -83,6 +84,7 @@
                 { data: 'role', name: 'users.role'},
                 { data: 'sub_name', name: 'subscriptions.name'},
                 { data: 'created_at', name: 'users.created_at'},
+                { data: 'last_signin', name: 'users.last_signin'},
                 { data: 'login', name: 'login', orderable: false, searchable: false},
                 { data: 'edit', name: 'edit', orderable: false, searchable: false},
                 { data: 'delete', name: 'delete', orderable: false, searchable: false},
