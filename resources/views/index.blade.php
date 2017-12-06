d48dec214c433b286a1cb0822c03478b70eda6ea
Artem Slepets <49h4DhPERFECT@gmail.com>
2017-12-06 23:30:06 +0200

    Scroll functionality has been added to playlists

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -41,27 +41,29 @@
           @foreach($playlists as $playlist)
               @if($playlist->videos->count())
                   <h1 class="my-4"><a href="{{$playlist->link ? route('category.show', $playlist->link): '#'}}">{{$playlist->name}}</a></h1>
-                  <div class="row">
-                    @foreach($playlist->videos as $video)
-                          <div class="col-lg-4 col-sm-6 portfolio-item">
-                              <div class="card h-100">
-                                  <a href="{{ route('video.show', $video->slug) }}"><img class="card-img-top" src="{{ $video->getThumbnail() }}" alt=""></a>
-                                  <div class="card-body">
-                                      <div class="row">
-                                          <div class="col-3">
-                                              <a href="{{ route('video.show', $video->slug) }}"><img src="{{ $video->user->thumbnail_path }}" class="rounded-circle img-fluid"></a>
-                                          </div>
-                                          <div class="col-9">
-                                              <h4>
-                                                  <a href="{{ route('video.show', $video->slug) }}">{{ $video->name }}</a>
-                                              </h4>
-                                              <p class="card-text">Created by: <a href="{{ route('channel.index', $video->user->slug) }}">{{$video->user->fullName()}}</a></p>
+                  <div class="row mx-2 mx-sm-0">
+                      <div class="owl-carousel owl-theme">
+                          @foreach($playlist->videos as $video)
+                              <div class="portfolio-item">
+                                  <div class="card h-100">
+                                      <a href="{{ route('video.show', $video->slug) }}"><img class="card-img-top" src="{{ $video->getThumbnail() }}" alt=""></a>
+                                      <div class="card-body">
+                                          <div class="row">
+                                              <div class="col-3">
+                                                  <a href="{{ route('video.show', $video->slug) }}"><img src="{{ $video->user->thumbnail_path }}" class="rounded-circle img-fluid"></a>
+                                              </div>
+                                              <div class="col-9">
+                                                  <h4>
+                                                      <a href="{{ route('video.show', $video->slug) }}">{{ $video->name }}</a>
+                                                  </h4>
+                                                  <p class="card-text">Created by: <a href="{{ route('channel.index', $video->user->slug) }}">{{$video->user->fullName()}}</a></p>
+                                              </div>
                                           </div>
                                       </div>
                                   </div>
                               </div>
-                          </div>
-                      @endforeach
+                          @endforeach
+                      </div>
                   </div>
               @endif
       @endforeach
