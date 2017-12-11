054333c2aa011997e29eb5fd4c90f8c0c020fe2f
dmitriyk-hyuna <dmitriy.krol@hyuna.bb>
2017-12-11 20:02:09 +0200

    Added seeder for csv

--- database/seeds/AdminSeeder.php
+++ database/seeds/AdminSeeder.php
@@ -12,24 +12,19 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-//        $users = User::whereRaw('LENGTH(first_name) > 31')->get();
-//
-//
-//        foreach ($users as $user) {
-//            list($name) = explode('@',$users[0]->email);
-//
-//            $user->first_name = $name;
-//            if ($user->save()) {
-//                Storage::disk('local')->append('public/index.csv', $user->id.','.$user->email);
-//            }
-//        }
+        $users = User::all();
 
-        $user = new User;
-        $user->first_name = 'John';
-        $user->last_name = 'Doe';
-        $user->email = 'admin@admin.com';
-        $user->password = bcrypt('123123');
-        $user->is_admin = true;
-        $user->save();
+
+        foreach ($users as $user) {
+            Storage::disk('local')->append('public/index.csv', $user->id.','.$user->first_name.','.$user->email.','.$user->created_at);
+        }
+
+//        $user = new User;
+//        $user->first_name = 'John';
+//        $user->last_name = 'Doe';
+//        $user->email = 'admin@admin.com';
+//        $user->password = bcrypt('123123');
+//        $user->is_admin = true;
+//        $user->save();
     }
 }
