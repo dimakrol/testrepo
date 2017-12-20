8811bcb1e3157bf88781e05780d127378c739d63
dmitriyk-hyuna <dmitriy.krol@hyuna.bb>
2017-12-20 17:31:01 +0200

    Updated.

--- resources/views/video/view.blade.php
+++ resources/views/video/view.blade.php
@@ -4,8 +4,8 @@
     <meta property="og:type"          content="website" />
     <meta property="og:title"         content="Your Website Title" />
     <meta property="og:description"   content="Your description" />
-    <meta property="og:image" content="{{ $gVideo->video_url }}"/>
-    <meta property="og:image:secure_url" content="{{ $gVideo->video_url }}" />
+    <meta property="og:image" content="{{ $gVideo->video->getThumbnail() }}"/>
+    <meta property="og:image:secure_url" content="{{ $gVideo->video->getThumbnail() }}" />
 @endsection
 
 @section('content')
