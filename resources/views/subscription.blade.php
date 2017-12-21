704c882c78c9108c2ac3ed2eeb6b54e5d8e2e47f
Artem Slepets <49h4DhPERFECT@gmail.com>
2017-12-21 16:35:52 +0200

    Prices have been fixed for the subscription page

--- resources/views/subscription.blade.php
+++ resources/views/subscription.blade.php
@@ -20,8 +20,8 @@
                         </div>-->
                         <div class="row no-gutters subscription__title special-offer" data-slogan="Currently - 25% Off!!!">
                             <div class="col-6 col-sm-5 text-center subscription__with-vr">
-                                <p class="subscription__price special-offer__price" data-special-price="{{$plan->stripe_id != 'yearlyuk' ? '$' : '&pound;' }}12">
-                                    {{$plan->stripe_id != 'yearlyuk' ? '$' : '&pound;' }}{{ $plan->amountInCurrency() }}
+                                <p class="subscription__price special-offer__price" data-special-price="{{$plan->stripe_id != 'yearlyuk' ? '$' : '&pound;' }}{{ $plan->amountInCurrency() }}">
+                                    {{$plan->stripe_id != 'yearlyuk' ? '$18' : '&pound;12' }}
                                 </p>
                                 <p class="subscription__term">/year*</p>
                             </div>
