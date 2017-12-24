9107c37064e2ddb5ad3b0cba0af634a97c6b95fd
Artem Slepets <49h4DhPERFECT@gmail.com>
2017-12-24 22:45:04 +0200

    Email reset password page has been slightly adjusted

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -3,14 +3,13 @@
 @section('content')
 <div class="container flex-grow-1">
     <div class="row">
-        <div class="col-md-6 offset-md-3 form-margin-top">
+        <div class="col-md-6 offset-md-3 mt-5">
             @if (session('status'))
                 <div class="alert alert-success">
                     {{ session('status') }}
                 </div>
             @endif
-            <h3 class="font-weight-bold" align="center">Reset Password</h3>
-            <hr>
+            <h3 class="font-weight-bold mb-5" align="center">Reset Password</h3>
             <form class="form-horizontal" method="POST" action="{{ route('password.email') }}">
                 {{ csrf_field() }}
 
@@ -20,8 +19,7 @@
                         <small class="form-text text-muted text-danger"><span class="text-danger">{{ $errors->first('email') }}</span></small>
                     @endif
                 </div>
-                <button type="submit" class="btn btn-success btn-block">Send Reset Link</button>
-                <hr>
+                <button type="submit" class="custom-button custom-button--primary">Send Reset Link</button>
             </form>
         </div>
     </div>
