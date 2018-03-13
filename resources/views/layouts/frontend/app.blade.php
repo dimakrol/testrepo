7477aec41aebf7ed0cc310aaa9ab9ac612a70ff7
dmitriyk-hyuna <dmitriy.krol@hyuna.bb>
2018-03-13 19:05:33 +0200

    Added google tag manager.

--- resources/views/layouts/frontend/app.blade.php
+++ resources/views/layouts/frontend/app.blade.php
@@ -31,10 +31,12 @@
         );
         fbq('track', 'PageView');
     </script>
-    <noscript>
-        <img height="1" width="1" src="https://www.facebook.com/tr?id=112667282703000&ev=PageView&noscript=1"/>
-    </noscript>
-
+    <!--Google Tag Manager-->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PTP75K8');</script>
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -69,7 +71,7 @@
     </script>
     @endguest
 </head>
-<body>
+<body><noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PTP75K8" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <div id="app">
         @include('layouts.navigation.main')
         <div class="container frontend-flash-container">
